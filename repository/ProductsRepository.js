ac2d978b7dbcb72c6a6d11c1c2cad263e8af1607
Hernande Monteiro <monteiro1998@live.com>
2022-08-24 16:40:46 -0300

    delete *.js files for most better repository

--- repository/ProductsRepository.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductsRepository = void 0;
-const mongoose_1 = __importDefault(require("mongoose"));
-const ProductsSchema_1 = require("../models/ProductsSchema");
-exports.ProductsRepository = mongoose_1.default.model("products", ProductsSchema_1.ProductsSchema);
