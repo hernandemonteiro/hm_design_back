7ff39b18a66dfaa70a1563c2f7ff7e39c975311d
Hernande Monteiro <monteiro1998@live.com>
2022-09-16 14:55:22 -0300

    rmv coments to clean space in compiled files

--- dist/router/ProductsRouter.js
+++ dist/router/ProductsRouter.js
@@ -6,61 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const ProductsController_1 = __importDefault(require("../controllers/ProductsController"));
 const productsRouter = (0, express_1.default)();
-/*
- * @description this route get all products;
- * @return a JSON with all products;
- */
 productsRouter.route("/products").get((req, res) => {
     return ProductsController_1.default.getAll(req, res);
 });
-/*
- * @description this route get products limited by quantity and pages;
- * @params [page] [qtd] limits in page with a quantity;
- * @return a JSON with all products limited by pages and qtd;
- */
 productsRouter.route("/products/:page/:qtd").get((req, res) => {
     return ProductsController_1.default.get(req, res);
 });
-/*
- * @description this route get one product by id;
- * @param [id] find one product by _id;
- * @return a JSON with one product;
- */
 productsRouter.route("/product/:id").get((req, res) => {
     return ProductsController_1.default.getById(req, res);
 });
-/*
- * @description this route delete one product by id;
- * @param [id] find the product to delete;
- */
 productsRouter.route("/product/:id").delete((req, res) => {
     return ProductsController_1.default.deleteProduct(req, res);
 });
-/*
- * @description this route register a product;
- * @param [name] name of the product;
- * @param [price] price of the product;
- * @param [images] images in array format;
- * @param [description] description of the product;
- * @param [category] category of the product for menu;
- * @param [status] status for view in store ('Ok' | 'In Progress');
- * @param [options] product options in array format;
- */
 productsRouter
     .route("/product/:name/:price/:images/:description/:category/:status/:options")
     .put((req, res) => {
     return ProductsController_1.default.registerProduct(req, res);
 });
-/*
- * @description this route update a product;
- * @param [id] find the product to update;
- * @param [name] name of the product;
- * @param [price] price of the product;
- * @param [images] images in array format;
- * @param [description] description of the product;
- * @param [status] status for view in store ('Ok' | 'In Progress');
- * @param [options] product options in array format;
- */
 productsRouter
     .route("/product/update/:id/:name/:price/:images/:description/:status/:options")
     .put((req, res) => {
--- dist/router/UserRouter.js
+++ dist/router/UserRouter.js
@@ -6,66 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const UsersController_1 = __importDefault(require("../controllers/UsersController"));
 const userRouter = (0, express_1.default)();
-/*
- * @description this route get all users;
- * @return a JSON with all users;
- */
 userRouter.route("/users").get((req, res) => {
     return UsersController_1.default.getAll(req, res);
 });
-/*
- * @description this route get one user by id;
- * @param [id] to find the user;
- * @return a JSON with the user;
- */
 userRouter.route("/users/:id").get((req, res) => {
     return UsersController_1.default.getById(req, res);
 });
-/*
- * @description this route update one user by id;
- * @param [id] find the user to updates;
- * @param [name] update the name;
- * @param [email] update the email;
- * @param [password] update the password;
- * @return a JSON with a registered user;
- */
 userRouter.route("/users/update/:id/:name/:email/:password").put((req, res) => {
     return UsersController_1.default.updateUser(req, res);
 });
-/*
- * @description this route delete one user by id;
- * @param [id] find the user to delete;
- * @return a JSON with the deleted user;
- */
 userRouter.route("/users/:id").delete((req, res) => {
     return UsersController_1.default.deleteUser(req, res);
 });
-/*
- * @description this route register a user using parameters;
- * @param [name] name of the user;
- * @param [email] email of the user;
- * @param [password] password of the user;
- * @param [type] define 1 == user, 0 == Admin;
- * @return a JSON with status: success || Error: message
- */
 userRouter.route("/users/:name/:email/:password/:type").put((req, res) => {
     return UsersController_1.default.userRegister(req, res);
 });
-/*
- * @description login route, generate a JWT;
- * @param [email] used to find user;
- * @param [password] used to find user;
- * @return a JWT token;
- */
 userRouter.route("/login/:email/:password").get((req, res) => {
     return UsersController_1.default.login(req, res);
 });
-/*
- * @description login route, generate a JWT;
- * @param [email] used to find user;
- * @param [password] used to find user;
- * @return a JWT token;
- */
 userRouter.route("/forgotPassword/:email").post((req, res) => {
     return UsersController_1.default.forgotPassword(req, res);
 });
