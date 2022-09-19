f5a9e83fbc19713344d58de737004b9c2daaebb4
Hernande Monteiro <monteiro1998@live.com>
2022-09-19 01:04:45 -0300

    change the method security of api

--- StartUp.ts
+++ StartUp.ts
@@ -11,7 +11,6 @@ const cors = require("cors");
 class StartUp {
   public app: Application;
   private _db: Database = new Database();
-  private hashSecurity = `/${process.env.HASH_SECRET}/`;
   constructor() {
     this.app = express();
     this._db.createConnection();
@@ -19,13 +18,27 @@ class StartUp {
   }
 
   routes() {
-    this.app.use(cors({ origin: ["http://localhost:3000", "https://hm-design.vercel.app"] }));
-    this.app.use(this.hashSecurity, userRouter);
-    this.app.use(this.hashSecurity, productsRouter);
-    this.app.use(this.hashSecurity, cartRouter);
-    this.app.use(this.hashSecurity, orderRouter);
-    this.app.use(this.hashSecurity, categoryRouter);
-    this.app.use(this.hashSecurity, forgotPasswordRouter);
+    this.app.use(express.json());
+    this.app.use("*", function (req, res, next) {
+      if (req.body.user == process.env.EMAIL_HM && req.body.pass == process.env.EMAIL_PASSWORD) {
+        next();
+      } else {
+        var err = new Error("You are not authenticated!");
+        res.setHeader("WWW-Authenticate", "Basic");
+        return next(err);
+      }
+    });
+    this.app.use(
+      cors({
+        origin: ["http://localhost:3000", "https://hm-design.vercel.app"],
+      })
+    );
+    this.app.use("/", userRouter);
+    this.app.use("/", productsRouter);
+    this.app.use("/", cartRouter);
+    this.app.use("/", orderRouter);
+    this.app.use("/", categoryRouter);
+    this.app.use("/", forgotPasswordRouter);
   }
 }
 
--- dist/StartUp.js
+++ dist/StartUp.js
@@ -15,19 +15,31 @@ const cors = require("cors");
 class StartUp {
     constructor() {
         this._db = new db_1.default();
-        this.hashSecurity = `/${process.env.HASH_SECRET}/`;
         this.app = (0, express_1.default)();
         this._db.createConnection();
         this.routes();
     }
     routes() {
-        this.app.use(cors({ origin: ["http://localhost:3000", "https://hm-design.vercel.app"] }));
-        this.app.use(this.hashSecurity, UserRouter_1.default);
-        this.app.use(this.hashSecurity, ProductsRouter_1.default);
-        this.app.use(this.hashSecurity, CartRouter_1.default);
-        this.app.use(this.hashSecurity, OrderRouter_1.default);
-        this.app.use(this.hashSecurity, CategoryRouter_1.default);
-        this.app.use(this.hashSecurity, ForgotPasswordRouter_1.default);
+        this.app.use(express_1.default.json());
+        this.app.use("*", function (req, res, next) {
+            if (req.body.user == process.env.EMAIL_HM && req.body.pass == process.env.EMAIL_PASSWORD) {
+                next();
+            }
+            else {
+                var err = new Error("You are not authenticated!");
+                res.setHeader("WWW-Authenticate", "Basic");
+                return next(err);
+            }
+        });
+        this.app.use(cors({
+            origin: ["http://localhost:3000", "https://hm-design.vercel.app"],
+        }));
+        this.app.use("/", UserRouter_1.default);
+        this.app.use("/", ProductsRouter_1.default);
+        this.app.use("/", CartRouter_1.default);
+        this.app.use("/", OrderRouter_1.default);
+        this.app.use("/", CategoryRouter_1.default);
+        this.app.use("/", ForgotPasswordRouter_1.default);
     }
 }
 exports.default = new StartUp();
