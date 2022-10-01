6ea9f4202cd12e38d6d08389c19cd5a04db61de2
Hernande Monteiro <monteiro1998@live.com>
2022-09-30 21:07:40 -0300

    vercel deploy

--- dist/__tests__/cart.test.js
+++ dist/__tests__/cart.test.js
@@ -3,29 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const CartController_1 = __importDefault(require("../controllers/CartController"));
+const CartService_1 = __importDefault(require("../services/CartService"));
 const globals_1 = require("@jest/globals");
-(0, globals_1.it)("Get", () => {
-    const getCart = CartController_1.default.get;
-    (0, globals_1.expect)(getCart);
-});
-(0, globals_1.it)("Get All", () => {
-    const getCart = CartController_1.default.getAll;
-    (0, globals_1.expect)(getCart);
-});
-(0, globals_1.it)("Get By ID", () => {
-    const getCart = CartController_1.default.getById;
-    (0, globals_1.expect)(getCart);
-});
-(0, globals_1.it)("Insert a Product in cart", () => {
-    const getCart = CartController_1.default.registerProductCart;
-    (0, globals_1.expect)(getCart);
-});
-(0, globals_1.it)("Delete a product of cart", () => {
-    const getCart = CartController_1.default.deleteProductCart;
-    (0, globals_1.expect)(getCart);
-});
-(0, globals_1.it)("Update a Product of cart", () => {
-    const getCart = CartController_1.default.updateProductCart;
-    (0, globals_1.expect)(getCart);
+(0, globals_1.describe)("cart", () => {
+    (0, globals_1.it)("Get products with a limit", () => {
+        const getAllWithLimit = CartService_1.default.getAllWithLimit(1, 10);
+        (0, globals_1.expect)(getAllWithLimit);
+    });
+    (0, globals_1.it)("Get All products", () => {
+        const getAll = CartService_1.default.getAll();
+        (0, globals_1.expect)(getAll);
+    });
+    (0, globals_1.it)("Update a Product of cart", () => {
+        const getCart = CartService_1.default.updateProductCart;
+        (0, globals_1.expect)(getCart);
+    });
 });
