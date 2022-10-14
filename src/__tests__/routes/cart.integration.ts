833a31e1f0a250478ad448c3e6a99103e8a0c253
Hernande Monteiro <monteiro1998@live.com>
2022-10-13 23:00:37 -0300

    mock process.env

--- src/__tests__/routes/cart.integration.ts
+++ src/__tests__/routes/cart.integration.ts
@@ -4,30 +4,35 @@ import StartUp from "../../StartUp";
 import Database from "../../infra/db";
 import sinon from "sinon";
 import { CartRepository } from "../../repository/CartRepository";
-import dotenv from 'dotenv';
-dotenv.config();
+import { dotEnvMock } from "../utils/factory";
+import dotEnv from "dotenv";
+dotEnv.config();
 
 jest.mock("../../infra/db");
+jest.mock("dotenv");
 
 describe("cart integration test", () => {
+  dotEnvMock();
+
   beforeAll(() => {
     jest.mocked(Database.createConnection);
+    process.env.HASH_SECRET = "tester";
     StartUp.app.listen(8585, () => console.log("on air!"));
   });
+
   it("get all products", async () => {
     sinon.stub(CartRepository, "find").returns({ status: "success" });
     const fetchCart = await fetch("http://localhost:8585/cart", {
       method: "GET",
       headers: {
         // how i use the secrets to fix error in github?
-        "x-access-token": process.env.HASH_TEST,
+        "x-access-token": "/lZunqpAUMGBXlJ6fP2JSg==",
       },
     })
       .then((Response) => Response.json())
       .then((Response) => {
         return Response;
       });
-      console.log(fetchCart)
-    // expect(fetchCart.status).toBe("success");
+    expect(fetchCart.status).toBe("success");
   });
 });
